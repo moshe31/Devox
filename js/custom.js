47bfeef9f18786d8a9e9bd0b76f33c9958e6cf7a
Moshe <moshedev31@gmail.com>
2024-03-31 06:22:23 -0400

    WElcome to devox Section Udpated
    Updated the Floating and scroll in animtion

--- js/custom.js
+++ js/custom.js
@@ -56,24 +56,6 @@ jQuery(document).ready(function () {
             menuToggle.classList.toggle('change')
         })
     }
-    if (window.matchMedia("(min-width: 768px)").matches) {
-        if (document.querySelector('.skewing-section')) {
-            let firstPanel = document.querySelector('.skewing-section');
-            gsap.to(firstPanel, {
-                scrollTrigger: {
-                    trigger: firstPanel,
-                    start: '100px 100px',
-                    end: 'bottom top',
-                    onEnter: () => {
-                        header.classList.add('dark')
-                    },
-                    onLeaveBack: () => {
-                        header.classList.remove('dark')
-                    }
-                }
-            });
-        }
-    }
     // if (window.matchMedia("(max-width: 767px)").matches) {
     if (document.querySelector('.intro-section')) {
         let firstPanel = document.querySelector('.intro-section');
@@ -103,9 +85,10 @@ jQuery(document).ready(function () {
                 })
             }
         })
-
+    }
+    if (window.matchMedia("(min-width: 992px)").matches) {
         gsap.utils.toArray('.default-panel').forEach(panel => {
-            gsap.from(panel.querySelectorAll('.default-panel-bg:not(:first-child)'), {
+            gsap.from(panel.querySelectorAll('.bg-lg .default-panel-bg-lg:not(:first-child)'), {
                 opacity: 0,
                 stagger: 0.2,
                 scrollTrigger: {
@@ -117,8 +100,23 @@ jQuery(document).ready(function () {
             })
         })
     }
+    else {
+        gsap.utils.toArray('.default-panel').forEach(panel => {
+            gsap.from(panel.querySelectorAll('.bg-sm .default-panel-bg-sm:not(:first-child)'), {
+                opacity: 0,
+                stagger: 0.2,
+                scrollTrigger: {
+                    trigger: panel,
+                    scrub: 1,
+                    // pin: true,
+                    start: '100px bottom',
+                    markers: true
+                }
+            })
+        })
+    }
     // }
-    
+
     // const cardsTl2 = gsap.timeline({
     //     scrollTrigger: {
     //         trigger: '.intro-section',
@@ -128,7 +126,7 @@ jQuery(document).ready(function () {
     //     defaults: {
     //         duration: 1.5
     //     }
-    
+
     // })
     const cardsTl = gsap.timeline({
         scrollTrigger: {
@@ -136,55 +134,57 @@ jQuery(document).ready(function () {
             start: '55% bottom',
             end: 'bottom top'
         },
-        defaults : {
+        defaults: {
             duration: 2
+        },
+        onComplete: () => {
+            let cards = Array.from(document.querySelectorAll('.intro-section1 .hovering-card'))
+            cards.forEach(card => {
+                gsap.to(card, {
+                  duration: Math.random() * 2 + 1, // Random duration between 1 and 3 seconds
+                  y: 10,
+                  ease: "power1.inOut",
+                  repeat: -1,
+                  yoyo: true
+                });
+              });
         }
-        // onComplete: () => {
-        //     gsap.to('.intro-section .hovering-card', {
-        //         scrollTrigger: {
-        //             trigger: '.intro-section',
-        //             scrub: true,
-        //             start: 'centre bottom'
-        //         },
-        //         y: 200
-        //     })
-        // }
     })
     cardsTl
-    .from('.intro-section1 h2', {
-        y: 150,
-        duration: 1,
-        opacity: 0,
-    })
-    .from('.intro-section1 .hovering-card.black', {
-        xPercent: -50,
-        yPercent: -50,
-        opacity: 0,
-        ease: 'power4.inOut'
-    }, 0)
-    .from('.intro-section1 .hovering-card.smoke', {
-        yPercent: -50,
-        opacity: 0,
-        ease: 'power4.inOut'
-    }, 0)
-    .from('.intro-section1 .hovering-card.brown', {
-        xPercent: 50,
-        yPercent: -50,
-        opacity: 0,
-        ease: 'power4.inOut'
-    }, 0)
-    .from('.intro-section1 .hovering-card.gray', {
-        xPercent: -50,
-        yPercent: 50,
-        opacity: 0,
-        ease: 'power4.inOut'
-    }, 0)
-    .from('.intro-section1 .hovering-card.white', {
-        xPercent: 50,
-        yPercent: 50,
-        opacity: 0,
-        ease: 'power4.inOut'
-    }, 0);
+        .from('.intro-section1 h2', {
+            y: 150,
+            duration: 1,
+            opacity: 0,
+        })
+        .from('.intro-section1 .hovering-card.black', {
+            xPercent: -50,
+            // yPercent: -50,
+            opacity: 0,
+            ease: 'power4.inOut'
+        }, 0)
+        .from('.intro-section1 .hovering-card.smoke', {
+            yPercent: -50,
+            opacity: 0,
+            ease: 'power4.inOut'
+        }, 0)
+        .from('.intro-section1 .hovering-card.brown', {
+            xPercent: 50,
+            // yPercent: -50,
+            opacity: 0,
+            ease: 'power4.inOut'
+        }, 0)
+        .from('.intro-section1 .hovering-card.gray', {
+            xPercent: -50,
+            // yPercent: 50,
+            opacity: 0,
+            ease: 'power4.inOut'
+        }, 0)
+        .from('.intro-section1 .hovering-card.white', {
+            xPercent: 50,
+            // yPercent: 50,
+            opacity: 0,
+            ease: 'power4.inOut'
+        }, 0);
     // const cardsTl2 = gsap.timeline({
     //     scrollTrigger: {
     //         trigger: '.intro-section2',
